e0e075b27c526a7391e959e96707f8352e47e585
LipePrado013 <lipe.praado@gmail.com>
2023-08-08 15:21:10 -0300

    criação de um novo site com tailwind

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css tailwind -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Five Hands</title>
+</head>
+
+<body class="">
+
+    <header class="myNav bg-yellow-900 shadow-md shadow-slate-500    ">
+        <nav class=" p-3 flex justify-between items-center">
+
+            <img class="border" src="img/logo-removebg-preview.png" width="150rem">
+            
+            <ul class="flex gap-12 p-3 ">
+                <li><a href="#1" class="hover:bg-yellow-700 p-2 rounded-md transition-all font-bold text-slate-100">Home</a>
+                </li>
+                <li><a href="#2" class="hover:bg-yellow-700 p-2 rounded-md transition-all font-bold text-slate-100">Sobre</a>
+                </li>
+                <li><a href="#3" class="hover:bg-yellow-700 p-2 rounded-md transition-all font-bold text-slate-100">Contato</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="myMain  flex justify-center ">
+        <div class="container mt-5 p-1 flex-wrap flex justify-evenly ">
+            <div class='border border-blue-800 w-80 p-2  text-center flex-col m-3 rounded-lg gap-2 shadow-md hover:shadow-green-500 hover:scale-105 transition-all flex items-center'>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28 ">Comprar +</button>
+            </div>
+            <div class='border border-blue-800 w-80 p-2 flex text-center flex-col m-3 rounded-lg shadow-md hover:shadow-green-500 hover:scale-105 transition-all gap-2 items-center '>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28">Comprar +</button>
+            </div>
+            <div class='border border-blue-800 w-80 p-2 flex text-center flex-col m-3 rounded-lg shadow-md hover:shadow-green-500 hover:scale-105 transition-all gap-2 items-center'>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28">Comprar +</button>
+            </div>
+            <div class='border border-blue-800 w-80 p-2 flex text-center flex-col m-3 rounded-lg shadow-md hover:shadow-green-500 hover:scale-105 transition-all gap-2 items-center'>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28">Comprar +</button>
+            </div>
+            <div class='border border-blue-800 w-80 p-2 flex text-center flex-col m-3 rounded-lg shadow-md hover:shadow-green-500 hover:scale-105 transition-all gap-2 items-center'>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28">Comprar +</button>
+            </div>
+            <div class='border border-blue-800 w-80 p-2 flex text-center flex-col m-3 rounded-lg shadow-md hover:shadow-green-500 hover:scale-105 transition-all gap-2 items-center'>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28">Comprar +</button>
+            </div>
+            <div class='border border-blue-800 w-80 p-2 flex text-center flex-col m-3 rounded-lg shadow-md hover:shadow-green-500 hover:scale-105 transition-all gap-2 items-center'>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28">Comprar +</button>
+            </div>
+            <div class='border border-blue-800 w-80 p-2 flex text-center flex-col m-3 rounded-lg shadow-md hover:shadow-green-500 hover:scale-105 transition-all gap-2 items-center'>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28">Comprar +</button>
+            </div>
+            <div class='border border-blue-800 w-80 p-2 flex text-center flex-col m-3 rounded-lg shadow-md hover:shadow-green-500 hover:scale-105 transition-all gap-2 items-center'>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28">Comprar +</button>
+            </div>
+            <div class='border border-blue-800 w-80 p-2 flex text-center flex-col m-3 rounded-lg shadow-md hover:shadow-green-500 hover:scale-105 transition-all gap-2 items-center'>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28">Comprar +</button>
+            </div>
+            <div class='border border-blue-800 w-80 p-2 flex text-center flex-col m-3 rounded-lg shadow-md hover:shadow-green-500 hover:scale-105 transition-all gap-2 items-center'>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28">Comprar +</button>
+            </div>
+            <div class='border border-blue-800 w-80 p-2 flex text-center flex-col m-3 rounded-lg shadow-md hover:shadow-green-500 hover:scale-105 transition-all gap-2 items-center'>
+                <img src="img/grafico1.png" width="100%">
+                <h1>{item um}</h1>
+                <p>{R$ 00.99}</p>
+                <button class="p-2 rounded-md font-bold bg-green-600 hover:bg-green-400 w-28">Comprar +</button>
+            </div>
+        </div>
+
+    </main>
+
+
+</body>
+
+</html>
